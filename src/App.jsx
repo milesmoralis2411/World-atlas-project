e4b38a84a27edc8c3c26127332f528cb5feb134b
Varun Mundada <varunmundada971@gmail.com>
2025-04-17 10:24:15 +0530

    Added Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,29 @@
- const App = () =>{
+import './App.css'
+import { Home } from './pages/Home'
+import { About } from './pages/About'
+import { Country } from './pages/Country'
+import { Contact } from './pages/Contact'
+
+const router = createBrowserRouter([
+  {
+    path : '/',
+    element : <Home/>
+  },
+  {
+    path : 'about',
+    element : <About/>
+  },
+  {
+    path : 'contact',
+    element : <Contact/>
+  },
+  {
+    path : 'country',
+    element : <Country/>
+  }
+])
+
+const App = () =>{
   return (<h1>Hello</h1>)
 }
 export default App
