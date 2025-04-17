841645e2310c2ba6c140c83799384a033608e05f
Varun Mundada <varunmundada971@gmail.com>
2025-04-17 13:19:15 +0530

    Changed title , added faviocn

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,5 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    git add .
-    git commit -m "Update feature"
-    git push
-
   </StrictMode>,
 )
